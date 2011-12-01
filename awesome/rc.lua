dbf532246e5b54c68099ba190e1e930e484def74
Valentin Serrago <ogarres@gmail.com>
2011-12-01 10:29:11 -0500

    commented out unused layouts

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -39,10 +39,10 @@ layouts =
     awful.layout.suit.fair,             -- 6
     awful.layout.suit.fair.horizontal,  -- 7
     --awful.layout.suit.spiral,           -- 8
-    awful.layout.suit.spiral.dwindle,   -- 9
-    awful.layout.suit.max,              -- 10
-    awful.layout.suit.max.fullscreen,   -- 11
-    awful.layout.suit.magnifier         -- 12
+    --awful.layout.suit.spiral.dwindle,   -- 9
+    --awful.layout.suit.max,              -- 10
+    --awful.layout.suit.max.fullscreen,   -- 11
+    --awful.layout.suit.magnifier         -- 12
 }
 -- }}}
 
